f7ded3f20bf9c8854149be0a965b97fdff0204d8
Nithin Krishnamurthi <nithin.krishnamurthi401@gmail.com>
2016-02-07 15:09:24 -0800

    Make a basic User model

--- Documents/rails/rails_projects/sample_app/app/controllers/users_controller.rb
+++ Documents/rails/rails_projects/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class UsersController < ApplicationController
   def new
   end
+  
+  private
+    def user_params
+      params.require(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ Documents/rails/rails_projects/sample_app/app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  
+  has_secure_password
+  before_save {|user| user.email = email.downcase}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:name, presence: true, length: {maximum: 50})
+  validates(:email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true)
+  validates(:password, presence: true, length:{minimum: 6})
+  validates(:password_confirmation, presence: true)
+end
